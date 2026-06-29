2f38b73609f6b9a28464071879ac9e1e1456881c
bangnaraj-boop <bang.naraj@gmail.com>
2026-06-29 08:34:04 -0700

    Added lastdate
    Added lastdate

--- Exercise2_fixed.py
+++ Exercise2_fixed.py
@@ -51,13 +51,14 @@ import argparse
 
 
 class PersonRecord:
-    def __init__(self, id, last_name, first_name, birthdate, role, hiredate):
+    def __init__(self, id, last_name, first_name, birthdate, role, hiredate, lastdate):
         self.id = id
         self.last_name = last_name
         self.first_name = first_name
         self.birthdate = birthdate
         self.role = role
         self.hiredate = hiredate
+        self.lastdate = lastdate
 
     def __repr__(self):
         return (
@@ -67,7 +68,8 @@ class PersonRecord:
             f"first_name='{self.first_name}', "
             f"birthdate={self.birthdate}, "
             f"role='{self.role}', "
-            f"hiredate={self.hiredate}"
+            f"hiredate={self.hiredate},
+            f"lastdate={self.lastdate}"
         )
 
 
